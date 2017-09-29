dcd55b7e301a9f14fbb33d7cdf42fc3011bf4b30
elbowkoala <eck325@nyu.edu>
2017-09-28 22:40:15 -0400

    ran YT's code for 1-event-per code

--- YT/process.m
+++ YT/process.m
@@ -13,12 +13,14 @@ result=zeros(300,800,961);
 
 
 for k=1:961
-    
-A=zeros(302,802);
+if rem(k,50) == 0
+    disp(['on YT scan ',num2str(k)])
+end   
+    A=zeros(302,802);
     
     for m=1:300
         for n=1:800
-            if cones(m,n,2)~=0
+            if cones(m,n,k)~=0
                 A(m+1,n+1)=1;
             end
         end
